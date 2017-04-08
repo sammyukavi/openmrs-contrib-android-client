f975c14ec69261309b86728ecd1cc013719b5dbd
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2017-04-08 23:34:33 +0300

    server url

--- openmrs-client/src/main/java/org/openmrs/mobile/utilities/ApplicationConstants.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/utilities/ApplicationConstants.java
@@ -27,7 +27,8 @@ public abstract class ApplicationConstants {
     public static final String VISIT_TYPE_UUID = "visit_type_uuid";
     public static final String LAST_SESSION_TOKEN = "last_session_id";
     public static final String LAST_LOGIN_SERVER_URL = "last_login_server_url";
-    public static final String DEFAULT_OPEN_MRS_URL = "http://devtest04.openmrs.org:8080/openmrs";
+	public static final String DEFAULT_OPEN_MRS_URL = "http://192.168.2.32:8080/openmrs"; //discuss on using the openhmis
+	// demo as the main test server to avoid changes here.
 
     public abstract static class OpenMRSSharedPreferenceNames {
         public static final String SHARED_PREFERENCES_NAME = "shared_preferences";

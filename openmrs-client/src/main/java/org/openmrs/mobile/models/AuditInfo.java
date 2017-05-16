8f3f8f4f98caa1625d1f49a2e8abfb9262bcdb95
Sammy N Ukavi Jr <sammyukavi@gmail.com>
2017-05-16 05:29:59 +0300

    Merge branch 'code_review' of https://github.com/OpenHMIS/openmrs-contrib-android-client into feature/patientdashboard
    # Conflicts:
    #	openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/PatientDashboardFragment.java
    #	openmrs-client/src/main/res/layout/toolbar_base_activity.xml

--- openmrs-client/src/main/java/org/openmrs/mobile/models/AuditInfo.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/models/AuditInfo.java
@@ -1,8 +1,5 @@
 package org.openmrs.mobile.models;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 import java.io.Serializable;
 
 public class AuditInfo extends BaseOpenmrsEntity implements Serializable {

bde30500b9e1874acc67a95114c984533277cffb
Andrew <andrewndungu@gmail.com>
2017-05-21 00:54:00 +0300

    Add Visit Image description

--- openmrs-client/src/main/java/org/openmrs/mobile/utilities/ObservationDeserializer.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/utilities/ObservationDeserializer.java
@@ -24,6 +24,7 @@ import com.google.gson.JsonParseException;
 
 import org.openmrs.mobile.models.Concept;
 import org.openmrs.mobile.models.Observation;
+import org.openmrs.mobile.models.Person;
 
 import java.lang.reflect.Type;
 
@@ -33,6 +34,7 @@ public class ObservationDeserializer implements JsonDeserializer<Observation> {
 	private static final String DISPLAY_KEY = "display";
 	private static final String VALUE_KEY = "value";
 	private static final String COMMENT_KEY = "comment";
+	private static final String DATE_KEY = "obsDatetime";
 
 	@Override
 	public Observation deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
@@ -44,11 +46,16 @@ public class ObservationDeserializer implements JsonDeserializer<Observation> {
 		observation.setUuid(jsonObject.get(UUID_KEY).getAsString());
 		observation.setDisplay(jsonObject.get(DISPLAY_KEY).getAsString());
 
-		JsonElement conceptJson = jsonObject.get("concept");
-
 		if(jsonObject.get(COMMENT_KEY) != JsonNull.INSTANCE && jsonObject.get(COMMENT_KEY) != null) {
 			observation.setComment(jsonObject.get(COMMENT_KEY).getAsString());
 		}
+
+		if(jsonObject.get(DATE_KEY) != JsonNull.INSTANCE && jsonObject.get(DATE_KEY) != null) {
+			observation.setObsDatetime(jsonObject.get(DATE_KEY).getAsString());
+		}
+
+		JsonElement conceptJson = jsonObject.get("concept");
+
 		if (conceptJson != null && "Visit Diagnoses".equals(conceptJson.getAsJsonObject().get(DISPLAY_KEY).getAsString())) {
 			JsonArray diagnosisDetailJSONArray = jsonObject.get("groupMembers").getAsJsonArray();
 			for (int i = 0; i < diagnosisDetailJSONArray.size(); i++) {
@@ -83,6 +90,15 @@ public class ObservationDeserializer implements JsonDeserializer<Observation> {
 			concept.setUuid(conceptJson.getAsJsonObject().get(UUID_KEY).getAsString());
 			observation.setConcept(concept);
 		}
+
+		JsonElement personJson = jsonObject.get("person");
+		if(personJson != null) {
+			Person person = new Person();
+			person.setUuid(personJson.getAsJsonObject().get(UUID_KEY).getAsString());
+			person.setDisplay(personJson.getAsJsonObject().get(DISPLAY_KEY).getAsString());
+			observation.setPerson(person);
+		}
+
 		return observation;
 	}
 

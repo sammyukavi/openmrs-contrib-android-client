22f5a5ea5d4a4d4fd79a47831e2792eb5f474e34
Andrew <andrewndungu@gmail.com>
2017-05-22 18:53:31 +0300

    Fix LayoutInflater NPE

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/VisitsRecyclerAdapter.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/VisitsRecyclerAdapter.java
@@ -48,7 +48,6 @@ public class VisitsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 	private List<Visit> visits;
 	private ObsDataService observationDataService;
 	private CustomDialogBundle createEditVisitNoteDialog;
-	LayoutInflater layoutInflater;
 	private ImageView showVisitDetails;
 	private Intent intent;
 
@@ -290,8 +289,7 @@ public class VisitsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 
 	private void addVisitNoteField(LinearLayout observationsContainer, String visitUUid, @Nullable Observation
 			observation) {
-		View row = layoutInflater
-				.inflate(R.layout.visit_obervation_row, null);
+		View row = LayoutInflater.from(context).inflate(R.layout.visit_obervation_row, null);
 		observationsContainer.addView(row);
 
 		Observation newObservation = new Observation();

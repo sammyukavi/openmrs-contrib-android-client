1448c7dc0320993dc5c62a65fa43336f4e684b04
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2017-05-27 18:35:09 +0300

    Started moving the login functionality to our rest services

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/data/db/impl/SessionDbService.java
@@ -0,0 +1,11 @@
+package org.openmrs.mobile.data.db.impl;
+
+import org.openmrs.mobile.data.BaseMetadataDataService;
+import org.openmrs.mobile.data.db.BaseDbService;
+import org.openmrs.mobile.data.db.BaseEntityDbService;
+import org.openmrs.mobile.data.db.DbService;
+import org.openmrs.mobile.models.Session;
+
+
+public class SessionDbService extends BaseDbService<Session> implements DbService<Session> {
+}

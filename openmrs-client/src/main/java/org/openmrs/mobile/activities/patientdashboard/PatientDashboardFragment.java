21407005bef0f14533f9cc1a56229ccd444e6cb7
Sammy N Ukavi Jr <sammyukavi@gmail.com>
2017-06-06 13:20:18 +0300

    Fixed active visit spacing Justified duration for closed visits Fixed font styling Show patient address on patient header

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/PatientDashboardFragment.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/PatientDashboardFragment.java
@@ -25,7 +25,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -175,10 +174,6 @@ public class PatientDashboardFragment extends ACBaseFragment<PatientDashboardCon
 	@Override
 	public void updateActiveVisitCard(List<Visit> visits) {
 
-		if (visits.size() == 0) {
-
-		}
-
 		for (Visit visit : visits) {
 			if (!StringUtils.notNull(visit.getStopDatetime())) {
 				startVisitButton.setVisibility(View.GONE);
@@ -281,7 +276,7 @@ public class PatientDashboardFragment extends ACBaseFragment<PatientDashboardCon
 
 	@Override
 	public void upDateProgressBar(boolean show) {
-		savingProgressBar.setVisibility(show ? View.VISIBLE : View.GONE);
+		//savingProgressBar.setVisibility(show ? View.VISIBLE : View.GONE);
 	}
 
 	@Override
--- openmrs-client/src/main/java/org/openmrs/mobile/data/impl/LoginDataService.java
+++
@@ -1,8 +0,0 @@
-package org.openmrs.mobile.data.impl;
-
-/**
- * Created by bandahealth on 6/6/17.
- */
-
-public class LoginDataService {
-}

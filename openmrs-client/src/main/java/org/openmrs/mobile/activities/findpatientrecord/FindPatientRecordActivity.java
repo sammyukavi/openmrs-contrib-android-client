4e60c8f6a929308abb22d312ee4161a7bad45a83
Kevin Burnett <kwburnett@gmail.com>
2017-08-02 14:59:14 +0300

    fixing an issue where the search icon doesn't display and sometimes doesn't work; also updated the search icon to be clearer and all white; lastly, made the search bar stretch the width of the activity

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/findpatientrecord/FindPatientRecordActivity.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/findpatientrecord/FindPatientRecordActivity.java
@@ -116,8 +116,7 @@ public class FindPatientRecordActivity extends ACBaseActivity {
 		}
 
 		setTitle(R.string.nav_find_patient);
-		mFindPatientMenuItem.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM |
-				MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+		mFindPatientMenuItem.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
 
 		searchPatientsView.setText(query);
 		searchPatientsView.addTextChangedListener(new TextWatcher() {

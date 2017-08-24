7755b31ac90f15b7c53df9c0303aec446161201e
Kevin Burnett <kwburnett@gmail.com>
2017-08-24 15:08:33 +0300

    fixing build issues and abstracting event fields

--- openmrs-client/src/main/java/org/openmrs/mobile/event/SyncPullEvent.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/event/SyncPullEvent.java
@@ -2,15 +2,9 @@ package org.openmrs.mobile.event;
 
 import android.support.annotation.Nullable;
 
-public class SyncPullEvent {
+public class SyncPullEvent extends SyncEvent {
 
-	public final String itemName;
-	public final String message;
-	public final int totalItemsToPull;
-
-	public SyncPullEvent(String message, @Nullable String itemName, @Nullable Integer totalItemsToPull) {
-		this.itemName = itemName;
-		this.message = message;
-		this.totalItemsToPull = totalItemsToPull;
+	public SyncPullEvent(String message, @Nullable String entity, @Nullable Integer totalItemsToPush) {
+		super(message, entity, totalItemsToPush);
 	}
 }
--- openmrs-client/src/main/java/org/openmrs/mobile/event/SyncPushEvent.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/event/SyncPushEvent.java
@@ -2,15 +2,9 @@ package org.openmrs.mobile.event;
 
 import android.support.annotation.Nullable;
 
-public class SyncPushEvent {
-
-	public final String entity;
-	public final String message;
-	public final int totalItemsToPush;
+public class SyncPushEvent extends SyncEvent {
 
 	public SyncPushEvent(String message, @Nullable String entity, @Nullable Integer totalItemsToPush) {
-		this.entity = entity;
-		this.message = message;
-		this.totalItemsToPush = totalItemsToPush;
+		super(message, entity, totalItemsToPush);
 	}
 }

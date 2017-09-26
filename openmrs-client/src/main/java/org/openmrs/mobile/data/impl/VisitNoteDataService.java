c7d4d8402b67f5a34308f9b1f727131604ed65af
Andrew <andrewndungu@gmail.com>
2017-09-26 13:29:48 +0300

    andr-270 Add diagnosis when offline -> turn on internet -> sync -> throws NPE on Dashboard

--- openmrs-client/src/main/java/org/openmrs/mobile/data/impl/VisitNoteDataService.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/data/impl/VisitNoteDataService.java
@@ -39,11 +39,14 @@ public class VisitNoteDataService extends BaseDataService<VisitNote, VisitNoteDb
 				}, () -> restService.save(visitNote),
 				(e) -> {
 					// void existing obs
-					if(visitNote.getEncounter() != null) {
+					if (visitNote.getEncounter() != null) {
 						obsDbService.voidExistingObs(visitNote.getEncounter());
 					}
 					// save new obs
+					e.getEncounter().processRelationships();
 					encounterDbService.save(e.getEncounter());
+					// remove visit note
+					dbService.delete(visitNote.getUuid());
 				});
 	}
 

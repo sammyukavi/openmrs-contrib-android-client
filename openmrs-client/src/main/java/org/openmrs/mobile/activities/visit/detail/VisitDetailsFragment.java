a415610331413684a48409ae892d0b485adf856b
Andrew <andrewndungu@gmail.com>
2017-09-27 12:27:43 +0300

    andr-273 Visit attributes not being voided while working offline (results to dups)

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/visit/detail/VisitDetailsFragment.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/visit/detail/VisitDetailsFragment.java
@@ -272,11 +272,12 @@ public class VisitDetailsFragment extends BaseDiagnosisFragment<VisitContract.Vi
 			}
 		} else {
 			for (VisitAttribute visitAttribute : visit.getAttributes()) {
-				loadVisitAttributeType(visitAttribute, visitAttributeTypes);
-				createVisitAttributeLayout(visitAttribute);
+				if (!visitAttribute.getVoided()) {
+					loadVisitAttributeType(visitAttribute, visitAttributeTypes);
+					createVisitAttributeLayout(visitAttribute);
+				}
 			}
 		}
-
 	}
 
 	@Override

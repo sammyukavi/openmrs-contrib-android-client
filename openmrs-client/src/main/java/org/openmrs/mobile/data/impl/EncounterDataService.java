e47887dfa040a115b000ce3fadd6eea2d175d0ce
Kevin Burnett <kwburnett@gmail.com>
2017-09-28 14:50:42 +0300

    ensuring Observations get voided and are removed from the DB appropriately

--- openmrs-client/src/main/java/org/openmrs/mobile/data/impl/EncounterDataService.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/data/impl/EncounterDataService.java
@@ -6,9 +6,13 @@ import android.support.annotation.Nullable;
 import org.openmrs.mobile.data.BaseDataService;
 import org.openmrs.mobile.data.PagingInfo;
 import org.openmrs.mobile.data.QueryOptions;
+import org.openmrs.mobile.data.RequestStrategy;
 import org.openmrs.mobile.data.db.impl.EncounterDbService;
+import org.openmrs.mobile.data.db.impl.ObsDbService;
 import org.openmrs.mobile.data.rest.impl.EncounterRestServiceImpl;
 import org.openmrs.mobile.models.Encounter;
+import org.openmrs.mobile.models.Observation;
+import org.openmrs.mobile.models.SyncAction;
 
 import java.util.List;
 
@@ -17,8 +21,13 @@ import javax.inject.Inject;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class EncounterDataService extends BaseDataService<Encounter, EncounterDbService, EncounterRestServiceImpl> {
+
+	private ObsDbService observationDbService;
+
 	@Inject
-	public EncounterDataService() { }
+	public EncounterDataService(ObsDbService observationDbService) {
+		this.observationDbService = observationDbService;
+	}
 
 	public void getByVisit(@NonNull String visitUuid, @Nullable QueryOptions options, @Nullable PagingInfo pagingInfo,
 			@NonNull GetCallback<List<Encounter>> callback) {
@@ -28,5 +37,59 @@ public class EncounterDataService extends BaseDataService<Encounter, EncounterDb
 		executeMultipleCallback(callback, options, pagingInfo,
 				() -> dbService.getByVisit(visitUuid, options, pagingInfo),
 				() -> restService.getByVisit(visitUuid, options, pagingInfo));
+	}
+
+	@Override
+	public void create(@NonNull Encounter entity, @NonNull GetCallback<Encounter> callback) {
+		checkNotNull(entity);
+		checkNotNull(callback);
+
+		QueryOptions options = new QueryOptions.Builder().requestStrategy(RequestStrategy.REMOTE_THEN_LOCAL).build();
+		executeSingleCallback(callback, options,
+				() -> {
+					entity.processRelationships();
+					Encounter result = dbService.save(entity);
+					syncLogService.save(result, SyncAction.NEW);
+					return result;
+				},
+				() -> restService.create(entity),
+				(Encounter result) -> {
+					if (!entity.getObs().isEmpty()) {
+						for (Observation observation : entity.getObs()) {
+							if (observation.getVoided() == true) {
+								observationDbService.delete(observation);
+							}
+						}
+					}
+					return result;
+				},
+				(e) -> dbService.save(e));
+	}
+
+	@Override
+	public void update(@NonNull Encounter entity, @NonNull GetCallback<Encounter> callback) {
+		checkNotNull(entity);
+		checkNotNull(callback);
+
+		QueryOptions options = new QueryOptions.Builder().requestStrategy(RequestStrategy.REMOTE_THEN_LOCAL).build();
+		executeSingleCallback(callback, options,
+				() -> {
+					entity.processRelationships();
+					Encounter result = dbService.save(entity);
+					syncLogService.save(result, SyncAction.UPDATED);
+					return result;
+				},
+				() -> restService.update(entity),
+				(Encounter result) -> {
+					if (!entity.getObs().isEmpty()) {
+						for (Observation observation : entity.getObs()) {
+							if (observation.getVoided() == true) {
+								observationDbService.delete(observation);
+							}
+						}
+					}
+					return result;
+				},
+				(e) -> dbService.save(e));
 	}
 }
